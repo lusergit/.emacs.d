9ac2c2c7a6ed4cd93987f0927d4ccc523b20e727
Luca Zaninotto (lusergit) <lucazanny@gmail.com>
2024-04-24 16:09:53 +0200

    modeline refresh

--- modules/modeline.el
+++ modules/modeline.el
@@ -3,13 +3,12 @@
 ;; The following code customizes the modeline
 
 ;;; Code:
-;; (use-package minions
-;;   :ensure)
+(use-package minions :ensure)
 
-;; (use-package mood-line
-;;   :ensure t
-;;   :config
-;;   (mood-line-mode))
+(use-package mood-line
+  :ensure t
+  :config
+  (mood-line-mode))
 
 (provide 'modeline)
 ;;; modeline.el ends here
