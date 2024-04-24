feb8640aee004ee91486cccbf694974bc193e491
Luca Zaninotto <luser@lHost.homenet.telecomitalia.it>
2024-04-24 10:22:19 +0200

    magit test

--- init.el
+++ init.el
@@ -8,33 +8,33 @@
 (setq user-email-address "lucazanny@gmail.com")
 
 ;; Set custom file
-(setq custom-file "~/.emacs.d/custom.el")
+(setq custom-file (concat user-emacs-directory "custom.el"))
 (load custom-file)
 
 ;; use-package init
 (require 'package)
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "https" "http")))
-  (when no-ssl (warn "No ssl!"))
-  (add-to-list 'package-archives
-               (cons
-		"melpa"
-		(concat proto "://melpa.org/packages/")) t))
+	    (not (gnutls-available-p))))
+(proto (if no-ssl "https" "http")))
+(when no-ssl (warn "No ssl!"))
+(add-to-list 'package-archives
+       (cons
+	"melpa"
+	(concat proto "://melpa.org/packages/")) t))
 (package-initialize)
 (setq use-package-always-ensure t)
 
 (unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+(package-refresh-contents)
+(package-install 'use-package))
 
 ;; also quelpa
 (use-package quelpa :ensure t)
 
 (setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
+`((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
+`((".*" ,temporary-file-directory t)))
 
 (defgroup lz-group nil
   "Customization group for my custom settings."
